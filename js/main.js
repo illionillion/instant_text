d1e759d82ba49229963f319b9b12532c349081e4
illionillion <yuto.0526.sano@gmail.com>
2021-08-30 00:19:48 +0900

    背景と文字色変えれるようにした。全選択は無理

--- js/main.js
+++ js/main.js
@@ -10,8 +10,7 @@ function take_ss(){
   });
 }
 
-function change_position(){
-  const ele=document.getElementById('position_select');
+function change_position(ele){
   //selectの選択されたoptionの番号を取得
   const get_num=ele.selectedIndex;
   console.log(get_num);
@@ -22,32 +21,58 @@ function change_position(){
   $('#target').css({"text-align":get_value});
 }
 
+
+function change_color(ele){
+
+  if(ele.name==='text_color'){
+    $('#target').css('color',ele.value);
+    return;
+  }
+  if(ele.name==='background_color'){
+    $('#target').css('background-color',ele.value);
+    return;
+  }
+  
+}
+
 window.addEventListener('DOMContentLoaded', ()=>{
   const target=document.querySelector('#target');
   target.addEventListener('focus',()=>{
     // console.log(target.innerHTML);
     if(target.innerHTML){
       const r=document.createRange();
-      // console.log(r)
+      console.log(r)
       r.setStart(target,0);
-      r.setEnd(target,1);
+      r.setEnd(target,target.childNodes.length);
+      console.log(target.childNodes.length);
+      console.log(r)
       window.getSelection().addRange(r);
     }
 
   });
 });
 
+
 window.onload=function(){
   document.getElementById('ss_btn').addEventListener('click',function(){
     take_ss();
   });
   document.getElementById('position_select').addEventListener('change',function(){
-    change_position();
+    change_position(this);
   });
   //後で質問
   // document.getElementById('target').addEventListener('focus',function(){
-  //   $(this).select();
+  //   // $(this).select();
+  //   // document.execCommand('SelectAll');//全て選択
   //   // this.innerHTML="";
+  //   //rangeオブジェクトの作成
+  //   const range = document.createRange();
+  //   //取得した要素の内側を範囲とする
+  //   range.selectNodeContents(this);
+  //   //範囲を選択状態にする
+  //   window.getSelection().addRange(range);
   //   console.log(this);
   // });
+  document.getElementById('text_color').addEventListener('change',function(){change_color(this);});
+  document.getElementById('background_color').addEventListener('change',function(){change_color(this);});
 }
